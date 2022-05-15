e2a2d3abd88f6203f3191c2cc246546344052b25
AbdulJH <abdul.chs21@gmail.com>
2022-05-15 16:32:27 -0500

    Created books object/constructor in app.js | Created look of website in index.html and slightly styled it in styles.css

---
+++ app.js
@@ -0,0 +1,16 @@
+function Books(title, author, pages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+  this.info = function() {
+    return ("The "+title+" by "+author+", "+pages+" pages, "+haveRead);
+  }
+}
+
+
+const theHungerGames = new Books("Hunger Games", "Suzanne Collins", 384, "not read yet");
+const sailorMoon = new Books("Sailor Moon", "Billy Collins", 222, "not read yet");
+
+
+console.log(theHungerGames.info());
